fc7079f7b202c5fd85ac801a1022f6e4ede13feb
Igor Feitosa de Araujo <igorlavras@hotmail.com>
2021-10-10 23:20:52 -0300

    :arrow_up: Improve fastify logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
+import { connect } from "v1/config/mongodb";
 import "reflect-metadata";
+import { setUserController } from "v1/api/user/user.controller";
+import { setTicketController } from "v1/api/ticket/ticket.controller";
 
 const PORT = 3000;
 
@@ -9,7 +12,14 @@ const server = async () => {
 		config();
 	}
 
-	const fastify = Fastify({ logger: true });
+	const fastify = Fastify({
+		logger: true,
+	});
+
+	await connect();
+
+	setUserController(fastify);
+	setTicketController(fastify);
 
 	fastify.listen(PORT, err => {
 		if (err) throw err;
