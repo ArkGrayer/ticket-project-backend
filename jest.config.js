64be7a9f2850d964b422f308409d2af2ee217e8e
Igor Feitosa de Araujo <igorlavras@hotmail.com>
2021-10-10 23:15:25 -0300

    :arrow_up: Upgraded some dependencies

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	moduleFileExtensions: ["js", "json", "ts"],
+	rootDir: "src",
+	testRegex: ".*\\.spec\\.ts$",
+	transform: {
+		"^.+\\.(t|j)s$": "ts-jest",
+	},
+	collectCoverageFrom: [
+		"**/*.ts",
+		"!tests/**/*.ts",
+		"!v*/api/**/*.entity.ts",
+		"!v*/api/**/*.controller.ts",
+		"!v*/config/*.ts",
+		"!v*/enum/*.ts",
+		"!index.ts",
+	],
+	setupFiles: ["./tests/setup.ts"],
+	coverageDirectory: "../coverage",
+	testEnvironment: "node",
+	moduleDirectories: ["node_modules", "src"],
+	resetMocks: true,
+	coverageThreshold: {
+		global: {
+			statements: 100,
+			branches: 100,
+			functions: 100,
+			lines: 100,
+		},
+	},
+};
