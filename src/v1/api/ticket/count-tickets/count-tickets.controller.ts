65e7c3b4475656542dbfed9f8ff12a3dc1ee93bf
ArkGrayer <igorlavras@hotmail.com>
2021-10-14 09:21:03 -0300

    Add count ticket route (#4)

---
+++ src/v1/api/ticket/count-tickets/count-tickets.controller.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { StatusCodeEnum } from "v1/enum/status-code";
+import { Route } from "v1/types/route";
+import { TicketEntity } from "../ticket.entity";
+import { countTickets } from "../count-tickets/count-tickets.service";
+
+export const ticketCountsController: Route = async (_request, reply) => {
+	let result;
+
+	try {
+		const ticketRepository = getRepository(TicketEntity);
+
+		result = await countTickets({
+			ticketRepository,
+		});
+	} catch (err: any) {
+		// eslint-disable-next-line no-console
+		console.error(err);
+
+		return reply.status(err.statusCode || StatusCodeEnum.INTERNAL).send({
+			err: err.message,
+		});
+	}
+
+	reply.send(result);
+};
